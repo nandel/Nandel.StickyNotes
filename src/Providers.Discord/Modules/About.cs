dd4cd7f4bae4d1e652e5cbd3d53bda33ae89c87e
Fernando Souza <fefso1909@gmail.com>
2021-11-08 17:55:34 -0300

    Link para o projeto no github

---
+++ src/Providers.Discord/Modules/About.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace Application.Discord.Modules
+{
+    public class About : ModuleBase<SocketCommandContext>
+    {
+        [Command("github")]
+        public async Task GithubAsync()
+        {
+            await ReplyAsync("https://github.com/nandel/Nandel.DiscordStickyNotes");
+        }
+    }
+}
