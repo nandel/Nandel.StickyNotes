7339149fc7d4833e4d0df03569a23419631e5602
Fernando Souza <fefso1909@gmail.com>
2021-11-09 03:14:01 -0300

    Operação de options/help para ajudar ao usuário aprender utilizar o bot

---
+++ src/Providers.Discord/Modules/Options.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace Application.Discord.Modules
+{
+    public class Options : ModuleBase<SocketCommandContext>
+    {
+        private static readonly string OptionsText = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "options.txt"));
+
+        [Command("options")]
+        [Alias("help")]
+        public async Task OptionsAsync()
+        {
+            await ReplyAsync(OptionsText);
+        }
+    }
+}
